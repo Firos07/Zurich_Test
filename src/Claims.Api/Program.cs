1f5af0f028d5432787998bc91f58afce57d74fb3
Prueba Zurich Dev <dev@prueba-zurich.local>
2026-08-14 19:02:21 -0600

    fix: accept enum values as strings in api

--- src/Claims.Api/Program.cs
+++ src/Claims.Api/Program.cs
@@ -4,7 +4,9 @@ using Claims.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddProblemDetails();
